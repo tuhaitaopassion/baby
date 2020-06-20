79ec5f1ff9ea0ce3b7c1b7d9b9e558ac1307fba9
tuhaitaopassion <1131938783@qq.com>
2020-06-20 12:16:46 +0800

    delete

--- springboot_es/src/main/java/cn/imooc/demo/springboot/es/repository/MySql/mysqlrespository.java
+++
@@ -1,6 +0,0 @@
-package cn.imooc.demo.springboot.es.repository.MySql;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface class mysqlrespository extends JpaRepository(m) {
-}
